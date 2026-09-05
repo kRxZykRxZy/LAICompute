1e2cf95f666b35d5e9b44035be859607637d7faa
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2026-09-05 13:27:27 +0100

    Make cache hierarchy runtime-detected instead of hardcoded

--- include/laic/cache.hpp
+++ include/laic/cache.hpp
@@ -1,18 +1,34 @@
 #pragma once
 #include <cstddef>
+#include <string>
 
 namespace laic {
 
+struct CpuCacheInfo {
+    size_t l1_data_bytes = 0;
+    size_t l1_instruction_bytes = 0;
+    size_t l2_bytes = 0;
+    size_t l3_bytes = 0;
+    size_t line_bytes = 0;
+    unsigned logical_cpus = 1;
+    std::string cpu_name;
+
+    static CpuCacheInfo detect();
+};
+
 struct CachePlan {
-    size_t l1_tile_bytes = 32 * 1024;
-    size_t l2_window_bytes = 256 * 1024;
-    size_t l3_window_bytes = 3 * 1024 * 1024;
-    size_t cache_line_bytes = 64;
+    CpuCacheInfo cpu;
+    size_t l1_tile_bytes = 0;
+    size_t l2_window_bytes = 0;
+    size_t l3_window_bytes = 0;
+    size_t cache_line_bytes = 0;
+
+    static CachePlan detect();
 };
 
 class CacheScheduler {
 public:
-    explicit CacheScheduler(CachePlan plan = {});
+    explicit CacheScheduler(CachePlan plan = CachePlan::detect());
     void prepare(const void* current, size_t current_bytes,
                  const void* next, size_t next_bytes,
                  const void* future, size_t future_bytes) const noexcept;
